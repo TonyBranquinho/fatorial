2ee79b432039bfb5a26ef55173abec785ba5d8f1
Tony <antony.branquinho@gmail.com>
2023-12-05 17:26:28 -0300

    precisei de ajuda

---
+++ Fatorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial {
+    class Program {
+        static void Main(string[] args) {
+
+            int n, i, fatorial;
+
+            Console.Write("Digite o valor de N: ");
+            n = int.Parse(Console.ReadLine());
+            
+            fatorial = 1;
+            for (i = 1; i <= n; i++) {
+                fatorial = fatorial * i;
+            }
+
+            Console.WriteLine("Fatorial = " + fatorial);
+        }
+    }
+}
